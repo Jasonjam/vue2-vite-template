8313257644c0d53fb0a9d30a62eff2bab558fed3
jasonjam <jasonjam66@gmail.com>
2023-07-24 13:18:44 +0000

    first init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>Vue 2</h1>
+        <HelloCompo />
+        <button @click="clickHandler()">{{ btnName }}</button>
+        <p>testsett</p>
+    </div>
+</template>
+
+<script>
+import HelloCompo from "./components/HelloCompo.vue";
+
+export default {
+    name: "App",
+    components: {
+        HelloCompo,
+    },
+    data() {
+        return {
+            btnName: "Btn test",
+        };
+    },
+    methods: {
+        clickHandler() {
+            console.log("Btn test Clicked");
+        },
+    },
+};
+</script>
+
+<style scoped>
+h1 {
+    font-size: 32px;
+    font-weight: bold;
+}
+</style>
---
+++ src/components/HelloCompo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="hello">
+        <h1>{{ helloMsg }}</h1>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            helloMsg: "Hello World",
+        };
+    },
+};
+</script>
+<style scope>
+.hello h1 {
+    color: limegreen;
+    font-size: 32px;
+}
+</style>
