c1722d912d8a481c58112d079840108c1ba107dd
jasonjam <jasonjam66@gmail.com>
2023-07-26 17:07:46 +0000

    不需要的指令，publicPath不需要此指令

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { createVuePlugin } from 'vite-plugin-vue2'
 
 export default {
     plugins: [createVuePlugin()],
+    publicPath: "./",
     server: {
         watch: {
             usePolling: true,
